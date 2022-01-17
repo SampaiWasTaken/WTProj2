461a557da5202561d929baadae7909447500215d
Leon Prüller <leprueller@edu.aau.at>
2022-01-17 18:34:38 +0100

    waiter view bisschen was

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -4,7 +4,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json',
-    Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NDIxNzA0NjMsImV4cCI6MTY0MjQyOTY2M30.JbNYJ1jtrvQBq_HYOEGsZIrxp5ylWliUBnFZySkfg_s'
+    Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NDI0Mzk3NDYsImV4cCI6MTY0MjY5ODk0Nn0.LicxB2BsOKrz2o9VjrUk7mQH90w3bKS2oyFwsg_ZswM'
   })
 };
 
@@ -22,4 +22,34 @@ export class APIService
   {
     return this.httpClient.get(`${this.API}/menuItems`, httpOptions)
   }
+  
+    getMenuItemByID(id: number)
+  {
+    return this.httpClient.get(`${this.API}/menuItems/${id}`, httpOptions)
+  }
+
+  getStatusDesc() //what each status number -> 1 = open,...
+  {
+    return this.httpClient.get(`${this.API}/orders/status`, httpOptions)
+  }
+
+  getOrders()
+  {
+    return this.httpClient.get(`${this.API}/orders`, httpOptions)
+  }
+
+  getRequests()
+  {
+    return this.httpClient.get(`${this.API}/guestrequest`, httpOptions)
+  }
+
+  
+  setRequest(request: any)
+  {
+    return this.httpClient.put(`${this.API}/guestrequest`, request, httpOptions).pipe()
+  }
+
+
+  
+
 }
