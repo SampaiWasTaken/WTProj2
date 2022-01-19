1545512f3c3e2f3a25c970876f9f2a03652bd29b
Leon Prüller <leprueller@edu.aau.at>
2022-01-19 19:58:06 +0100

    added waiter navigation

---
+++ src/app/components/waiter-nav/waiter-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-waiter-nav',
+  templateUrl: './waiter-nav.component.html',
+  styleUrls: ['./waiter-nav.component.css']
+})
+export class WaiterNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
