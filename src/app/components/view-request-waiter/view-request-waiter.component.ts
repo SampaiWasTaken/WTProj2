91dc05ecbe58ade2b87dbd8c2c49eab045a96db8
SampaiWasTaken <samuel.radler@htl-klu.at>
2022-02-15 11:41:05 +0100

    bugfixes

--- src/app/components/view-request-waiter/view-request-waiter.component.ts
+++ src/app/components/view-request-waiter/view-request-waiter.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { APIService } from '../../services/api.service';
 import { Request } from 'src/app/request';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
-import { Observable,Subscription, interval  } from 'rxjs';
+import { Observable, Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-view-request-waiter',
   templateUrl: './view-request-waiter.component.html',
   styleUrls: ['./view-request-waiter.component.css']
 })
-export class ViewRequestWaiterComponent implements OnInit {
+export class ViewRequestWaiterComponent implements OnInit
+{
   private updateSubscription?: Subscription;
 
   allRequests: any = []
@@ -18,37 +19,48 @@ export class ViewRequestWaiterComponent implements OnInit {
   constructor(private apiService: APIService,
     private cdr: ChangeDetectorRef, private _snackBar: MatSnackBar) { }
 
-  ngOnInit(): void {
-    this.updateSubscription = interval(5000).subscribe(
-      (val) => { this.apiService.getRequests().subscribe(res =>
+  ngOnInit(): void
+  {
+    if (localStorage.getItem('isLoggedIn') == "true")
+    {
+      this.updateSubscription = interval(5000).subscribe(
+        (val) =>
         {
-          let oldData = this.allRequests;
-          this.allRequests = res;
-          this.allRequests = this.allRequests.filter((o: any) => {return o.status === 1})
-         // console.log(this.allRequests)
-         if(oldData.length !== this.allRequests.length && oldData.length < this.allRequests.length){
-          this._snackBar.open("new Customer Request", "okay");
-         }
-    
-        })});
+          this.apiService.getRequests().subscribe(res =>
+          {
+            if (localStorage.getItem('isLoggedIn') == "true")
+            {
+              let oldData = this.allRequests;
+              this.allRequests = res;
+              this.allRequests = this.allRequests.filter((o: any) => { return o.status === 1 })
+              // console.log(this.allRequests)
+              if (oldData.length !== this.allRequests.length && oldData.length < this.allRequests.length)
+              {
+                this._snackBar.open("new Customer Request", "okay");
+              }
+            }
+          })
+        });
+    }
 
     this.apiService.getRequests().subscribe(res =>
-      {
-        this.allRequests = res;
-        this.allRequests = this.allRequests.filter((o: any) => {return o.status === 1})
-       // console.log(this.allRequests)
-  
-      })
+    {
+      this.allRequests = res;
+      this.allRequests = this.allRequests.filter((o: any) => { return o.status === 1 })
+      // console.log(this.allRequests)
+
+    })
   }
-  
-  requestProcessed(req: any){
 
-  // WHY ISNT THE *NGFOR NOT UPDATING ?!!?!?!?
-  //https://stackoverflow.com/questions/53203224/my-view-does-not-update-when-i-change-my-array-in-ngfor
-  //no idea seemed iteresting the Stackoverflow link
+  requestProcessed(req: any)
+  {
 
-  //BRUH WHY IS IT NOW WORKING WHEN I CALL NGONINIT AGAIN WTF
-    this.apiService.setRequest(req).subscribe( bruh => {this.ngOnInit()});
+    // WHY ISNT THE *NGFOR NOT UPDATING ?!!?!?!?
+    //https://stackoverflow.com/questions/53203224/my-view-does-not-update-when-i-change-my-array-in-ngfor
+    //no idea seemed iteresting the Stackoverflow link
+
+    //BRUH WHY IS IT NOW WORKING WHEN I CALL NGONINIT AGAIN WTF
+    this.apiService.setRequest(req).subscribe(bruh => { this.ngOnInit() });
 
   }
 
